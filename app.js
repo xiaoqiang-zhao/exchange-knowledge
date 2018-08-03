9883e21fc876a7ba8bd78fc9b740c0cd4f9e702d
xiaoqiang-zhao <longze_xq@163.com>
2018-08-03 20:05:43 +0800

    添加获取手机号获取功能

--- app.js
+++ app.js
@@ -1,6 +1,42 @@
 //app.js
 App({
-    onLaunch() {},
+    onLaunch() {
+        // 微信登录
+        wx.login({
+            success: function(res) {
+                if (res.code) {
+                    // 用微信登录返回的加密数据换取解密后的数据
+                    // wx.request({
+                    //     method: 'POST',
+                    //     url: 'https://www.liuliuke.com/huanhuan/login',
+                    //     data: {
+                    //         code: res.code
+                    //     },
+                    //     success(res) {
+                    //         console.log(res);
+                    //     }
+                    // });
+
+                    // wx.showToast({
+                    //     title: '登录成功',
+                    //     icon: 'success',
+                    //     duration: 2000
+                    // });
+                    console.log('loginCode:' + res.code);
+                    wx.setStorage({
+                        key: 'code',
+                        data: res.code
+                    });
+                } else {
+                    wx.showToast({
+                        title: '登录失败',
+                        icon: 'success',
+                        duration: 2000
+                    });
+                }
+            }
+        });
+    },
     globalData: {
         userInfo: null
     }
--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,9 +1,43 @@
 /**
- * @file 底部导航
+ * @file 我的
  * @author 小强赵
  */
 
 Page({
-    data: {},
-    onLoad() {}
+    data: {
+        str: ''
+    },
+    onLoad() {},
+    getPhoneNumber(e) {
+        console.log(e.detail.errMsg);
+        console.log(e.detail.iv);
+        console.log(e.detail.encryptedData);
+        wx.getStorage({
+            key: 'code',
+            success(res) {
+                // 换真数据
+                // 用微信登录返回的加密数据换取解密后的数据
+                const data = {
+                    code: res.data,
+                    iv: e.detail.iv,
+                    encryptedData: e.detail.encryptedData
+                };
+                console.log(data);
+                wx.request({
+                    method: 'POST',
+                    url: 'https://www.liuliuke.com/huanhuan/authMobile',
+                    data,
+                    success(res) {
+                        console.log(res);
+                    }
+                });
+            },
+            fail(res) {
+                console.log(res);
+            }
+        });
+        // this.setData({
+        //     str: 'iv:' + e.detail.iv
+        // });
+    }
 });
