898b398731e3349b8b29a64ae4a28a7c9df8b608
xiaoqiang-zhao <longze_xq@163.com>
2018-08-07 10:56:39 +0800

    添加获取附近的人页面

--- pages/search-nearby/search-nearby.js
+++ pages/search-nearby/search-nearby.js
@@ -3,9 +3,6 @@
  * @author 小强赵
  */
 
-let name = '';
-let idcard = '';
-
 Page({
     data: {
         disabled: true,
@@ -13,52 +10,18 @@ Page({
         idcard: ''
     },
     onLoad() {
-        // debugger
-        // getApp().globalData.code
+        this.getLocation();
     },
-    nameInput(e) {
-        name = e.detail.value;
-        this.validate();
-    },
-    idcardInput(e) {
-        idcard = e.detail.value;
-        this.validate();
-    },
-    validate() {
-        let disabled;
-        if (idcard.length === 18 && name.length > 1) {
-            disabled = false;
-        }
-        else {
-            disabled = true;
-        }
-        this.setData({
-            disabled
-        });
-    },
-    submit() {
-        // console.log({
-        //     token: getApp().globalData.code,
-        //     name,
-        //     idcard
-        // });
-        wx.request({
-            method: 'POST',
-            header: {
-                'content-type': 'application/x-www-form-urlencoded'
-            },
-            url: 'https://www.liuliuke.com/huanhuan/realname',
-            data: {
-                token: getApp().globalData.code,
-                name,
-                idcard
-            },
+    getLocation() {
+        wx.getLocation({
             success(res) {
                 wx.setStorage({
-                    key: 'mobile',
-                    data: res.data.mobile
+                    key: 'location',
+                    data: res
                 });
-                me.navigateTo();
+                // console.log(res);
+                // longitude 经度
+                // latitude 维度
             }
         });
     }
