84656386cf37a4358ceae9f773159f8a32cc3742
xiaoqiang-zhao <longze_xq@163.com>
2018-08-21 13:27:39 +0800

    添加公共函数：获取多字段 Storage，返回 Promise

---
+++ utils/common.js
@@ -0,0 +1,34 @@
+/**
+ * @file 业务无关公共函数
+ */
+
+export default {
+
+    /**
+     * 获取 Storage 存储的数据，支持多个字段
+     * 获取失败的字段不赋值
+     *
+     * @return {Object} Promise 对象
+     */
+    getStorageData(...keys) {
+        let count = 0;
+        const result = {};
+
+        return new Promise((resolve) => {
+            keys.forEach(item => {
+                wx.getStorage({
+                    key: item,
+                    success(res) {
+                        result[item] = res.data;
+                    },
+                    complete() {
+                        count++;
+                        if (count === keys.length) {
+                            resolve(result);
+                        }
+                    }
+                });
+            });
+        });
+    }
+};
