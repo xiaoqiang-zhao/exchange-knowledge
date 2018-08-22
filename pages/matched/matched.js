04f09edac6f9d25205a5d3fc73a4ae4937e74459
xiaoqiang-zhao <longze_xq@163.com>
2018-08-22 08:54:03 +0800

    添加“匹配页”功能

--- pages/matched/matched.js
+++ pages/matched/matched.js
@@ -2,8 +2,53 @@
  * @file 已配对
  * @author 小强赵
  */
+import commonUtil from '../../utils/common';
 
 Page({
-    data: {},
-    onLoad() {}
+    data: {
+        list: [
+            {
+                avatar: 'https://liuliuke.com/headPics/04082018/5b65224e76085.jpg',
+                career: '软件工程师',
+                time: '08/02',
+                nick: '杨官龙',
+                title: '软件工程师'
+            }
+        ]
+    },
+    onLoad() {
+        commonUtil.getStorageData('token').then(res => {
+            this.loadList({
+                token: res.token
+            });
+        });
+    },
+
+    loadList(data) {
+        const me = this;
+        wx.request({
+            method: 'GET',
+            url: 'https://www.liuliuke.com/huanhuan/matchList',
+            data,
+            success(res) {
+                me.setData({
+                    list: res.data.data.userList
+                });
+            }
+        });
+    },
+
+    setClipboard(event) {
+        const data = event.currentTarget.dataset.wxaccount;
+        wx.setClipboardData({
+            data,
+            success() {
+                wx.showToast({
+                    title: `已复制对方微信${data}`,
+                    icon: 'success',
+                    duration: 2000
+                });
+            }
+        });
+    }
 });
