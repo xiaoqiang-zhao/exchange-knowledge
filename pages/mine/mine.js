231c1f20a6c3277633011aa0d12c42c8fef9b78a
xiaoqiang-zhao <longze_xq@163.com>
2018-08-22 08:54:34 +0800

    添加“我的页”功能

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -2,15 +2,45 @@
  * @file 我的
  * @author 小强赵
  */
+import commonUtil from '../../utils/common';
 
 Page({
     data: {
-        str: ''
+        userInfo: {}
     },
     onLoad() {
-    },
+        commonUtil.getStorageData(
+            'headerImgSrc',
+            // 实名认证
+            'realName',
+            'idcard',
 
-    
-    
-    
+            // 个人信息
+            'career',
+            'wxaccount',
+
+            // 我的知识
+            'title',
+            'content'
+        ).then(res => {
+            this.setData({
+                userInfo: res
+            });
+        });
+    },
+    navigateToRealName() {
+        wx.navigateTo({
+            url: '/pages/real-name/real-name'
+        });
+    },
+    navigateToPersonalInfo() {
+        wx.navigateTo({
+            url: '/pages/personal-info/personal-info'
+        });
+    },
+    navigateToMyKnowledge() {
+        wx.navigateTo({
+            url: '/pages/my-knoledge/my-knoledge'
+        });
+    }
 });
